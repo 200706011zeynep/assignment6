eb854227989335340b18a57feb4ea3b5bd51f8ce
Zeynep Sude Altay <https://github.com/mu-se373-200706011/assignment5.git>
2022-04-06 11:38:28 +0300

    initial commit

--- app/src/main/java/com/zeynepaltay/theofficeweek5/SecondActivity.kt
+++ app/src/main/java/com/zeynepaltay/theofficeweek5/SecondActivity.kt
@@ -1,27 +1,30 @@
 package com.zeynepaltay.theofficeweek5
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-       val button3 = findViewById<Button>(R.id.button3)
+        val button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            val button5 = findViewById<Button>(R.id.button5)
+            button5.setOnClickListener {
+                val intent = Intent(this, login::class.java)
+                startActivity(intent)
 
+            }
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflator = MenuInflater(this)
-        inflator.inflate(R.menu.menu,menu)
-        return super.onCreateOptionsMenu(menu)
+        /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            val inflator = MenuInflater(this)
+            inflator.inflate(R.menu.menu, menu)
+            return super.onCreateOptionsMenu(menu)
+        }*/
     }
 }
