a173663c947304ed481c779d324aa63fa23e879f
Zeynep Sude Altay <https://github.com/mu-se373-200706011/assignment5.git>
2022-04-06 11:40:32 +0300

    initial commit

---
+++ app/src/main/java/com/zeynepaltay/theofficeweek5/login.kt
@@ -0,0 +1,19 @@
+package com.zeynepaltay.theofficeweek5
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class login :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_in)
+        val button4 = findViewById<Button>(R.id.button4)
+        button4.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+    }
+
+}}
